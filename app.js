671e4e138653fd00c48d086fbc64d2454b8d9de7
Guillermo Gutiérrez <guillegr123@gmail.com>
2018-10-22 10:48:34 -0600

    UI-3206: Remove deleted account breadcrumb (#26)
    * Remove deleted account breadcrumb
    
    * Remove empty prop
    
    Co-Authored-By: guillegr123 <guillegr123@gmail.com>

--- app.js
+++ app.js
@@ -1257,19 +1257,18 @@ define(function(require) {
 
 			contentTemplate.find('#accountsmanager_delete_account_btn').on('click', function(e) {
 				self.confirmDeleteDialog(accountData.name, function() {
-					self.callApi({
-						resource: 'account.delete',
+					self.deleteAccount({
 						data: {
 							accountId: accountData.id,
-							data: {},
 							generateError: false
 						},
 						success: function(data, status) {
 							parent.find('.main-content').empty();
 							parent.find('.account-list-element[data-id="' + accountData.id + '"]').remove();
+							parent.find('.account-browser-breadcrumbs .account-browser-breadcrumb').last().remove();
 						},
-						error: function(data, status) {
-							if (data.message === 'account_has_descendants') {
+						error: function(parsedError) {
+							if (parsedError.message === 'account_has_descendants') {
 								monster.ui.alert('error', self.i18n.active().account_has_descendants);
 							}
 						}
@@ -2207,6 +2206,21 @@ define(function(require) {
 				},
 				error: function(data, status) {
 					error && error(data);
+				}
+			});
+		},
+
+		deleteAccount: function(args) {
+			var self = this;
+
+			self.callApi({
+				resource: 'account.delete',
+				data: args.data,
+				success: function(data, status) {
+					args.hasOwnProperty('success') && args.success(data.data);
+				},
+				error: function(parsedError) {
+					args.hasOwnProperty('error') && args.error(parsedError);
 				}
 			});
 		}
