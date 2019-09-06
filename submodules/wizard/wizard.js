d89f24b444f2fec0b4687bd4840c23e9df3cdbd8
Guillermo Gutierrez <guillegr123@gmail.com>
2019-09-06 15:20:45 -0600

    Revert "Use validation plugin function to focus invalid"
    This reverts commit 0eed737c5670b1c829529fa6cdbb7b922ec5c2bf.

--- submodules/wizard/wizard.js
+++ submodules/wizard/wizard.js
@@ -264,6 +264,15 @@ define(function(require) {
 						template: $template
 					});
 
+					// Set static validations
+					monster.ui.validate($template.find('form'), {
+						rules: {
+							'accountInfo.accountRealm': {
+								realm: true
+							}
+						}
+					});
+
 					return $template;
 				};
 
@@ -284,14 +293,6 @@ define(function(require) {
 			var self = this,
 				$form = $template.find('form'),
 				isValid = false,
-				validator = monster.ui.validate($template.find('form'), {
-					focusInvalid: true,
-					rules: {
-						'accountInfo.accountRealm': {
-							realm: true
-						}
-					}
-				}),
 				generalSettingsData;
 
 			// Set dynamic validations
@@ -321,8 +322,6 @@ define(function(require) {
 				if (!_.has(generalSettingsData, 'accountAdmins')) {
 					generalSettingsData.accountAdmins = [];
 				}
-			} else {
-				validator.focusInvalid();
 			}
 
 			return {
