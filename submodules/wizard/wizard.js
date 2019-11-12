144ab75c7fe3cc02781db49b89d17703ccbf730d
Fernando González <fernandomenjivar2011@gmail.com>
2019-11-12 14:07:28 -0600

    [4.3] MACT-42: Set wizard default country from config file (#79)
    * Set wizard default country from config file
    
    * Refactor Set wizard default country

--- submodules/wizard/wizard.js
+++ submodules/wizard/wizard.js
@@ -232,6 +232,7 @@ define(function(require) {
 					correlative: 1
 				},
 				generalSettingsData = data.generalSettings,
+				defaultCountry = monster.config.whitelabel.countryCode,
 				initTemplate = function() {
 					var $template = $(self.getTemplate({
 							name: 'step-generalSettings',
@@ -249,7 +250,7 @@ define(function(require) {
 					monster.ui.countrySelector(
 						$countriesDropdown,
 						{
-							selectedValues: _.get(generalSettingsData, 'accountInfo.country', ''),
+							selectedValues: _.get(generalSettingsData, 'accountInfo.country', defaultCountry),
 							options: {
 								showEmptyOption: true
 							}
