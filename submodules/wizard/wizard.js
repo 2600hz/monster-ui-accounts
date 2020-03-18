9d20c122ea2512d901f0c76b12def65e2605769b
Guillermo Gutiérrez <guillegr123@gmail.com>
2020-03-18 08:19:19 -0600

    MACT-64: Save account realm and omit empty values (#94)

--- submodules/wizard/wizard.js
+++ submodules/wizard/wizard.js
@@ -623,9 +623,9 @@ define(function(require) {
 		 * @param  {jQuery} $template  Step template
 		 * @param  {Object} args  Wizard's arguments
 		 * @param  {Object} args.data  Wizard's data that is shared across steps
-		 * @param  {Object} args.eventArgs  Event arguments
-		 * @param  {Boolean} args.eventArgs.completeStep  Whether or not the current step will be
-		 *                                                completed
+		 * @param  {Object} eventArgs  Event arguments
+		 * @param  {Boolean} eventArgs.completeStep  Whether or not the current step will be
+		 *                                           completed
 		 * @returns  {Object}  Object that contains the updated step data, and if it is valid
 		 */
 		wizardAccountContactsUtil: function($template, args, eventArgs) {
@@ -1761,7 +1761,8 @@ define(function(require) {
 				technicalContact = accountContacts.technicalContact,
 				salesRepresentative = accountContacts.salesRep,
 				controlCenterFeatures = wizardData.creditBalanceAndFeatures.controlCenterAccess.features,
-				accountDocument = {
+				omitEmpty = _.partialRight(_.omitBy, _.isEmpty),
+				accountDocument = _.merge({
 					call_restriction: _
 						.mapValues(wizardData.usageAndCallRestrictions.callRestrictions, function(value) {
 							return {
@@ -1769,7 +1770,7 @@ define(function(require) {
 							};
 						}),
 					contact: {
-						billing: {
+						billing: omitEmpty({
 							country: accountInfo.country,
 							region: accountInfo.state,
 							locality: accountInfo.city,
@@ -1779,12 +1780,7 @@ define(function(require) {
 							email: billingContact.email,
 							name: billingContact.fullName,
 							number: billingContact.phoneNumber.e164Number
-						},
-						technical: {
-							email: technicalContact.email,
-							name: technicalContact.fullName,
-							number: technicalContact.phoneNumber.e164Number
-						}
+						})
 					},
 					language: accountInfo.language,
 					name: accountInfo.accountName,
@@ -1803,28 +1799,33 @@ define(function(require) {
 							})
 							.value()
 					}
-				};
-
-			// Set optional data
-			if (_.has(accountInfo, 'realm')) {
-				accountDocument.realm = accountInfo.realm;
-			}
-			if (_.has(salesRepresentative, 'contractEndDate')) {
-				var contractEndDateGregorian = self.wizardDateToGregorianWithCurrentTimeZone(salesRepresentative.contractEndDate);
-
-				_.set(accountDocument, 'contract.end_date', contractEndDateGregorian);
-			}
-			if (_.has(salesRepresentative, 'representative')) {
-				_.set(
-					accountDocument,
-					'contract.representative',
-					{
-						account_id: self.accountId,
-						user_id: salesRepresentative.representative.userId,
-						name: salesRepresentative.representative.fullName
+				},
+				technicalContact.isEmpty ? {} : {
+					contact: {
+						technical: omitEmpty({
+							email: technicalContact.email,
+							name: technicalContact.fullName,
+							number: technicalContact.phoneNumber.e164Number
+						})
 					}
-				);
-			}
+				},
+				_.has(accountInfo, 'accountRealm') ? {
+					realm: accountInfo.accountRealm
+				} : {},
+				_.has(salesRepresentative, 'contractEndDate') ? {
+					contract: {
+						end_date: self.wizardDateToGregorianWithCurrentTimeZone(salesRepresentative.contractEndDate)
+					}
+				} : {},
+				_.has(salesRepresentative, 'representative') ? {
+					contract: {
+						representative: {
+							account_id: self.accountId,
+							user_id: salesRepresentative.representative.userId,
+							name: salesRepresentative.representative.fullName
+						}
+					}
+				} : {});
 
 			return accountDocument;
 		},
