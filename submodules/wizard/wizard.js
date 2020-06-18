28f353335f2357a2cf755add7887acdd5c1d77f0
Guillermo Gutierrez <guillegr123@gmail.com>
2020-06-18 15:51:19 -0600

    Add pwd strength indicator in wizard's admin list

--- submodules/wizard/wizard.js
+++ submodules/wizard/wizard.js
@@ -505,6 +505,14 @@ define(function(require) {
 			counters.correlative += 1;
 			counters.index += 1;
 
+			monster.ui.showPasswordStrength(
+				$adminItemTemplate.find('[type="password"]'),
+				{
+					display: 'emoji',
+					showInitialStatus: true
+				}
+			);
+
 			self.wizardAppendListItem({
 				item: $adminItemTemplate,
 				listContainer: $listContainer,
